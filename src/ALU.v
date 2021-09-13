08d5769b0c4ae3fdf91d5dfbe8a681792ae985b7
macarena.gauna <macarena.gauna@craftech.io>
2021-09-13 16:10:39 -0300

    variable intermedia para asignar de tipo reg a tipo wire

--- src/ALU.v
+++ src/ALU.v
@@ -6,7 +6,7 @@ module ALU #(
   input wire [NB_OPERANDO - 1 : 0] dato_a,
   input wire [NB_OPERANDO - 1 : 0] dato_b,
   input wire [NB_OPCODE - 1 : 0] opcode,
-  output reg [NB_OUT - 1 : 0] out
+  output wire [NB_OUT - 1 : 0] out
 );
 
   localparam ADD = 6'b100000;
@@ -18,11 +18,15 @@ module ALU #(
   localparam SRL = 6'b000010;
   localparam NOR = 6'b100111;
 
+  reg [NB_OUT-1:0] result;
+
 
   always @(*) begin : ALU_operation
     case (opcode)
-      ADD: out = dato_a + dato_b;
+      ADD: result = dato_a + dato_b;
     endcase
   end
+
+  assign out =  result[NB_OUT:0];
 
 endmodule
